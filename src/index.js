824b65e36853cfb3599a988c20199c023fdff030
Igor Loskutov <igor.loskutoff@gmail.com>
2016-05-10 14:03:20 +0300

    refactor telegram quiz to new lib + separate file + use custom web server creation (so no future listener clutters)

--- src/index.js
+++ src/index.js
@@ -1,101 +1,14 @@
 import eliza from './elizaParse.js';
 // import server from './server.js';
-import telegram from './telegram.js';
-import Quiz from './quiz.js';
+import { webHookCallback as telegramWebHookCallback, isPolling } from './telegram.js';
+import TelegramQuiz from './telegramQuiz.js';
 const Promise = require('bluebird');
-const chunk = require('lodash/chunk');
+const http = require('http');
 
-// telegram-quiz related mappings
-let telegramQuizState = {
-  // {chat id: {telegram message id: 'question id', running: boolean, currentQuestion}}
-};
+if (!isPolling) {
+  const server = http.createServer(telegramWebHookCallback);
+  server.listen(process.env.PORT || 3000);
+  // server.use();
+}
 
-// method for formatting reply keyboard to square-like structure
-const makeSquare = a => chunk(a, Math.ceil(Math.sqrt(a.length)));
-
-// message is group message
-const isGroup = msg => msg.from.id !== msg.chat.id;
-
-const sendQuestion = (chatId, q) => {
-  const { answers } = q.question;
-  const { id: qid } = q;
-  return telegram.sendMessage(chatId, `${q.question.text}: \n
-  ${answers.map((a, i) => `/${i}: ${a.text}`).join('\n')}`, {
-    reply_markup: {
-      keyboard: makeSquare(answers.map((a, i) => `/${i}`)),
-      force_reply: true
-      //hide_keyboard: true,
-      //one_time_keyboard: true // todo
-      // selective: true // Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id) not actualy if several users
-    }
-  })
-    .then(sentMessage => {
-    const mid = sentMessage.message_id;
-    telegramQuizState[chatId].idMapping[mid] = q.id;
-    return sentMessage;
-  });
-};
-
-const sendQuestionStat = (chatId, givenAnswer, correctAnswer) => {
-  return telegram.sendMessage(chatId, correctAnswer !== givenAnswer ?
-    `Given answer: ${givenAnswer.text}; correct answer: ${correctAnswer.text}` :
-    `Correct!`);
-};
-
-const sendSuccess = (chatId, quiz) => {
-  const answerLog = quiz.answers;
-  const correct = answerLog.filter(({qid, aid}) => quiz.quiz.questions[qid].answers[aid].isCorrect).length;
-  return telegram.sendMessage(chatId, `End. Correct answers: ${correct}/${answerLog.length}`);
-};
-
-telegram.onText(/^\/start/, msg => { // TODO CHECK IF RUNNING
-  const chatId = msg.chat.id;
-  const fromId = msg.from.id;
-  const quiz = Quiz.initQuiz(chatId, fromId);
-  const initialQuestion = quiz.currentQuestion();
-  telegramQuizState[chatId] = {running: true, currentQuestion: initialQuestion, idMapping: {}};
-  sendQuestion(chatId, initialQuestion);
-  // init quiz, return first question
-});
-
-telegram.onText(/\/(\d+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-
-  const quiz = Quiz.getQuiz(chatId);
-  if (!quiz) return console.warn('got answer but no quiz initialised.'); // TODO error for user?
-
-  let questionId = undefined;
-
-  if (isGroup(msg)) { // additional checks for reply_to_message
-    const replyTo = msg.reply_to_message;
-    if (!replyTo) return console.warn('got answer without reply to'); // TODO error for user?
-    const replyToId = replyTo.message_id; // always here
-    const quizState = telegramQuizState[chatId];
-    if (!quizState) return console.error(`no quiz state for chat ${chatId}`, telegramQuizState);
-    questionId = quizState.idMapping[replyToId];
-    if (questionId === undefined) return console.warn(`no saved real question id for answer ${replyToId}`); // TODO error for user?
-  } else {
-    questionId = quiz.currentQuestion().id
-  }
-
-  const fromId = msg.from.id;
-  const answerId = Number(match[1]);
-
-  quiz.giveAnswer(fromId/*TODO if user who is not in quiz answered*/, questionId, answerId)
-    .then(({ nextQuestion, givenAnswer, correctAnswer }) => {
-      return sendQuestionStat(chatId, givenAnswer, correctAnswer).then(() => nextQuestion ?
-        sendQuestion(chatId, nextQuestion) : sendSuccess(chatId, quiz));
-    })
-
-});
-
-// just for testing Eliza without telegram involved
-// TODO if we have any server AND telegram api webhook they conflict.
-//server.route({
-//  method: 'GET',
-//  path: '/message/{user}/{message}',
-//  handler: function (request, reply) {
-//    const { user, message } = request.params;
-//    reply(eliza.parse(user, message));
-//  }
-//});
+new TelegramQuiz();
--- src/telegram.js
+++ src/telegram.js
@@ -1,27 +1,25 @@
 const token = process.env.TELEGRAM_TOKEN;
-const TelegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf');
 import env from './env.js';
 
-// TODO URL from env vars
-
-console.warn('env.isDevelopment()', env.isDevelopment(), 'token', token);
-
-const botConfig = (env.isDevelopment()) ? {polling: true} : { // turn it on for everything until HTTPS is
+const botConfig = (env.isDevelopment()) ? {polling: true} : {
   polling: false,
-  webHook: { // TODO NO webhook, NO polling as webhook conflicts with any type of server as it does http.listen itself
-    port: process.env.PORT,
-    host: '0.0.0.0' // sign cert to have https
-  }
+  webHook: true
 };
 
-const bot = new TelegramBot(token, botConfig);
-if (botConfig.webHook) {
+export const bot = new Telegraf(token);
+const webHookPath = `testbotserver.herokuapp.com/${token}`;
+export const isPolling = !botConfig.webHook;
+
+if (!isPolling) {
   bot.setWebHook('').then(() => {
-    bot.setWebHook(`https://testbotserver.herokuapp.com/${token}`);
+    bot.setWebHook(`https://${webHookPath}`);
   });
+} else {
+  bot.startPolling();
 }
 
-export default bot;
+export const webHookCallback = bot.webHookCallback(`/${webHookPath}`);
 
 // Matches /echo [whatever]
 //bot.onText(/\/echo (.+)/, function (msg, match) {
