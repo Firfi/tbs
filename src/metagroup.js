5d94a51e4e02beac106bde6679e741da1f6a2ee3
Igor Loskutov <igor.loskutoff@gmail.com>
2016-05-16 21:09:04 +0300

    leave group feature, user can join in group only once (not twice), relay mesage feature

--- src/metagroup.js
+++ src/metagroup.js
@@ -2,37 +2,81 @@ const Promise = require('bluebird');
 import { bot as telegram } from './telegram.js';
 
 let groups = {}; // {groupId: metaGroup}
+let groupsByUsers = {}; // {uid: {gid: {}}} // cache
 
 class MetaGroup {
-  constructor() {
-    this.uids = [];
+  constructor(id) {
+    this.id = id;
+    this.users = {}; // {uid: {user info?}}
   }
+
   join(uid) {
     return new Promise(success => {
-      this.uids.push(uid);
-      success(this);
+      // check if already joined. no sense to join again.
+      if (this.users[uid]) return success(false);
+      this.users[uid] = {};
+      if (!groupsByUsers[uid]) groupsByUsers[uid] = {};
+      groupsByUsers[uid][this.id] = this;
+      success(true);
+    });
+  }
+
+  leave(uid) { // will return falsy if no users really left
+    return new Promise(success => {
+      if (!this.users[uid]) return success(false); // nothing to leave but not a failure (suppose so?)
+      delete this.users[uid];
+      delete groupsByUsers[uid][this.id];
+      success(true);
     });
   }
 
   // proxy methods TODO generate on "meta" level
   sendMessage(message, props) {
-    this.uids.forEach(uid =>
-      telegram.sendMessage(uid, message, props)
-    )
+    return new Promise(success => {
+      // TODO asyncjs
+      Object.keys(this.users).forEach(uid => {
+        telegram.sendMessage(uid, message, props).catch(e => console.error(e));
+      });
+      success();
+    });
   }
   // proxy methods ^
 }
 
 export const getOrCreate = (id) => {
-  return new Promise((success) => {
-    success(groups[id] || (() => {
-      const g = new MetaGroup();
+  return get(id).then(group => {
+    return group || (() => {
+      const g = new MetaGroup(id);
       groups[id] = g;
       return g;
-    })());
+    })();
+  });
+};
+
+export const get = id => {
+  return new Promise(success => {
+    success(groups[id]);
   });
 };
 
 export const join = (gid, uid) => {
-  return getOrCreate(gid).then(group => group.join(uid));
+  return getOrCreate(gid).then(group => {
+    return group.join(uid).then(ok => {
+      return { ok, group };
+    });
+  });
+};
+
+export const leave = (gid, uid) => {
+  return get(gid).then(group => {
+    if (group) {
+      return group.leave(uid);
+    } // else nothing to leave. safe to ignore but actually may be wrong operation. most probably safe though
+  });
+};
+
+export const groupsFor = uid => {
+  return new Promise(success => {
+    success(groupsByUsers[uid] || {});
+  });
 };
--- src/subbots/relay/index.js
+++ src/subbots/relay/index.js
@@ -1,27 +1,47 @@
-import { join as joinMetaGroup } from '../../metaGroup.js';
+import { join as joinMetaGroup, groupsFor, get as getGroup } from '../../metaGroup.js';
 import { bot as telegram } from '../../telegram.js';
 
 export default
 class Relay {
   constructor() {
 
-    telegram.hears(/^\/join (\w+)/, function * () {
+    telegram.hears(/^\/join (\w+)/, function * () { // TODO generic functionality for all metaGroup bots. leave/join/etc
       this.state.done = true;
       const gid = this.match[1]; // new msg.chat.id actually
       const msg = this.message;
       const fromId = msg.from.id;
-      joinMetaGroup(gid, fromId).then(metaGroup => {
-        return metaGroup.sendMessage(`user ${fromId} joined`); // TODO user name
+      joinMetaGroup(gid, fromId).then(({ group, ok }) => {
+        if (ok) return group.sendMessage(`user ${fromId} joined`); // TODO user name
       });
-
     });
 
-    //telegram.on('message', function * (next) {
-    //  if (this.state.done) return;
-    //  const msg = this.message;
-    //  const fromId = msg.from.id;
-    //
-    //});
+    telegram.hears(/^\/leave (\w+)/, function * () {
+      this.state.done = true;
+      const gid = this.match[1];
+      const msg = this.message;
+      const fromId = msg.from.id;
+      getGroup(gid).then(group => {
+        if (group) {
+          return group.leave(fromId).then(ok => {
+            if (ok) {
+              group.sendMessage(`user ${fromId} left`);
+            }
+          });
+        }
+      });
+    });
+
+    telegram.on('message', function * () {
+      if (this.state.done) return;
+      const msg = this.message;
+      const fromId = msg.from.id;
+      groupsFor(fromId).then(groups => {
+        Object.keys(groups).forEach(gid => {
+          groups[gid].sendMessage(msg.text);
+        })
+
+      });
+    });
 
   }
 }
